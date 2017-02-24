16df88e3fbbef875873139c244897e0827c2385e
Vojtěch Bakaj <bakajvo1@fit.cvut.cz>
2017-02-24 16:13:17 +0100

    [renderEngine] Add testCube object

---
+++ src/renderEngine/objects/Cube.java
@@ -0,0 +1,100 @@
+package renderEngine.objects;
+
+import org.lwjgl.util.vector.Vector3f;
+import renderEngine.Loader;
+import renderEngine.entities.Entity;
+import renderEngine.models.TexturedModel;
+import renderEngine.textures.Texture;
+
+public class Cube {
+
+    private static final float[] vertices = {
+            -0.5f, 0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, 0.5f, -0.5f,
+
+            -0.5f, 0.5f, 0.5f,
+            -0.5f, -0.5f, 0.5f,
+            0.5f, -0.5f, 0.5f,
+            0.5f, 0.5f, 0.5f,
+
+            0.5f, 0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, 0.5f,
+            0.5f, 0.5f, 0.5f,
+
+            -0.5f, 0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f, 0.5f,
+            -0.5f, 0.5f, 0.5f,
+
+            -0.5f, 0.5f, 0.5f,
+            -0.5f, 0.5f, -0.5f,
+            0.5f, 0.5f, -0.5f,
+            0.5f, 0.5f, 0.5f,
+
+            -0.5f, -0.5f, 0.5f,
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, 0.5f
+
+    };
+
+    private static final float[] textureCoords = {
+
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0
+
+
+    };
+
+    private static final int[] indices = {
+            0, 1, 3,
+            3, 1, 2,
+            4, 5, 7,
+            7, 5, 6,
+            8, 9, 11,
+            11, 9, 10,
+            12, 13, 15,
+            15, 13, 14,
+            16, 17, 19,
+            19, 17, 18,
+            20, 21, 23,
+            23, 21, 22
+
+    };
+
+    private Entity entity;
+
+    public Cube(Loader loader, Vector3f position) {
+        TexturedModel model = new TexturedModel(loader.loadToVAO(vertices, textureCoords, indices), new Texture(loader.loadTexture("texture_512x512")));
+        entity = new Entity(model, position, 0, 0, 0, 1);
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+}
